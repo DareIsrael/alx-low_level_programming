a0b99115dd83c25b9e490c070f1cf2d1eb7730eb
DareIsrael <dareisrael4gmail.com>
2023-06-16 09:35:36 -0700

    Betty commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - A Programs that prints a line with put function
+*Return: 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
