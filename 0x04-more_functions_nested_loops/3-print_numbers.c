351454952d01869887bee588df1f97a6c84c71b9
DareIsrael <dareisrael4gmail.com>
2023-06-22 12:27:56 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,21 @@
 #include "main.h"
+
 /**
-* print_numbers - print the numbers from 0 to 9
-* Description: You can only use _putchar twice
-**/
+ * print_numbers - print the numbers from 0 to 9
+ * Description: You can only use _putchar twice
+ */
+
 void print_numbers(void)
 {
-char i;
+	int i;
 
-for (i = '0'; i <= '9'; i++)
-{
- _putchar(i);
-}
-_putchar('\n');
+	i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
 }
 
